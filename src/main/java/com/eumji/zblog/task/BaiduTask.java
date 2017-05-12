a9d248aad61db51bf5ab3a0cf7db047fcba49fdf
eumji025 <zombie_goo@yeah.net>
2017-05-12 23:31:49 +0800

    小改动

--- src/main/java/com/eumji/zblog/task/BaiduTask.java
+++ src/main/java/com/eumji/zblog/task/BaiduTask.java
@@ -7,11 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 /**
  * FILE: com.eumji.zblog.task.BaiduTask.java
@@ -28,13 +28,12 @@ public class BaiduTask {
     @Autowired
     private ArticleService articleService;
 
-    private URLConnection initConnect() throws IOException {
-        URLConnection conn = new URL(POST_URL).openConnection();
+    private HttpURLConnection initConnect() throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) new URL(POST_URL).openConnection();
         conn.setRequestProperty("Host","data.zz.baidu.com");
         conn.setRequestProperty("User-Agent", "curl/7.12.1");
         conn.setRequestProperty("Content-Length", "83");
         conn.setRequestProperty("Content-Type", "text/plain");
-
         conn.setDoInput(true);
         conn.setDoOutput(true);
         return conn;
@@ -52,16 +51,23 @@ public class BaiduTask {
      * @param ids
      * @throws IOException
      */
-    private void writerUrl(URLConnection conn,String... ids) throws IOException {
+    private void writerUrl(HttpURLConnection conn,String... ids) throws IOException {
         PrintWriter out=null;
+        BufferedReader bf = null;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < ids.length; i++){
             sb.append(BASE_URL+"/article/details/"+ids[i]+"\n");
         }
         logger.info("推送的url为:"+sb.toString());
+        conn.connect();
         out=new PrintWriter(conn.getOutputStream());
         out.print(sb.toString().trim());
-        out.close();
+        out.flush();
+        int code = conn.getResponseCode();
+        if (code == 200){
+            logger.info("the article url push success");
+        }
+
     }
 
     /**
